68a13d548d1de4761d67623e71459800b7610cd0
Deepali-Surlikar <47281054+Deepali-Surlikar@users.noreply.github.com>
2019-02-03 20:33:20 +0530

    dummy data to add in tables added added

---
+++ DBUtils/DataInsert.sql
@@ -0,0 +1,81 @@
+/*
+-- Query: SELECT * FROM test.symbol
+-- Date: 2019-02-03 20:25
+*/
+INSERT INTO `symbol` (`ID`,`Reference`,`Shape`,`CreatedOn`,`UpdatedOn`) VALUES (1,'1895eda1-26b3-11e9-bbf8-1215b6b719c2','Point','2019-02-02 06:22:33','2019-02-02 06:22:33');
+INSERT INTO `symbol` (`ID`,`Reference`,`Shape`,`CreatedOn`,`UpdatedOn`) VALUES (2,'189677a8-26b3-11e9-bbf8-1215b6b719c2','Polygon','2019-02-02 06:22:33','2019-02-02 06:22:33');
+
+
+
+
+
+/*
+-- Query: SELECT * FROM test.userprofile
+-- Date: 2019-02-03 20:25
+*/
+INSERT INTO `userprofile` (`ID`,`Reference`,`Firstname`,`Lastname`,`Age`,`ImageURL`,`CreatedOn`,`UpdatedOn`) VALUES (1,'6ea53818-26b1-11e9-bbf8-1215b6b719c2','Virat','Kohli',31,'http://p.imgci.com/db/PICTURES/CMS/263600/263697.20.jpg','2019-02-02 06:10:39','2019-02-02 06:10:39');
+INSERT INTO `userprofile` (`ID`,`Reference`,`Firstname`,`Lastname`,`Age`,`ImageURL`,`CreatedOn`,`UpdatedOn`) VALUES (2,'6ea5abc8-26b1-11e9-bbf8-1215b6b719c2','Suresh','Raina',30,'http://p.imgci.com/db/PICTURES/CMS/202700/202703.1.jpg','2019-02-02 06:10:39','2019-02-02 06:10:39');
+INSERT INTO `userprofile` (`ID`,`Reference`,`Firstname`,`Lastname`,`Age`,`ImageURL`,`CreatedOn`,`UpdatedOn`) VALUES (3,'6ea5ad59-26b1-11e9-bbf8-1215b6b719c2','Ajinkya','Rahane',29,'http://p.imgci.com/db/PICTURES/CMS/263600/263694.1.jpg','2019-02-02 06:10:39','2019-02-02 06:10:39');
+INSERT INTO `userprofile` (`ID`,`Reference`,`Firstname`,`Lastname`,`Age`,`ImageURL`,`CreatedOn`,`UpdatedOn`) VALUES (4,'6ea5add9-26b1-11e9-bbf8-1215b6b719c2','MS','Dhoni',37,'http://p.imgci.com/db/PICTURES/CMS/263700/263705.20.jpg','2019-02-02 06:10:39','2019-02-02 06:10:39');
+
+
+
+
+
+/*
+-- Query: SELECT * FROM test.project
+-- Date: 2019-02-03 20:23
+*/
+INSERT INTO `project` (`ID`,`Reference`,`Name`,`Description`,`Symbol`,`CreatedOn`,`UpdatedOn`) VALUES (1,'588bcaf0-26b0-11e9-bbf8-1215b6b719c2','Project School','Lorem Ipsum is simply dummy text of the printing and typesetting\n			industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500 s,\n			when an unknown printer took a galley of type and scrambled it to make a type specimen\n			book.',NULL,'2019-02-02 06:02:53','2019-02-02 06:02:53');
+INSERT INTO `project` (`ID`,`Reference`,`Name`,`Description`,`Symbol`,`CreatedOn`,`UpdatedOn`) VALUES (2,'ff4db4dd-26b0-11e9-bbf8-1215b6b719c2','Project College','Lorem Ipsum is simply dummy text of the printing and typesetting\n			industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500 s,\n			when an unknown printer took a galley of type and scrambled it to make a type specimen\n			book.',NULL,'2019-02-02 06:07:33','2019-02-02 06:07:33');
+INSERT INTO `project` (`ID`,`Reference`,`Name`,`Description`,`Symbol`,`CreatedOn`,`UpdatedOn`) VALUES (3,'02a93df0-26b1-11e9-bbf8-1215b6b719c2','Project University','Lorem Ipsum is simply dummy text of the printing and typesetting\n			industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500 s,\n			when an unknown printer took a galley of type and scrambled it to make a type specimen\n			book.',NULL,'2019-02-02 06:07:38','2019-02-02 06:07:38');
+INSERT INTO `project` (`ID`,`Reference`,`Name`,`Description`,`Symbol`,`CreatedOn`,`UpdatedOn`) VALUES (4,'2f7ec813-26b1-11e9-bbf8-1215b6b719c2','Project City','Lorem Ipsum is simply dummy text of the printing and typesetting\n			industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500 s,\n			when an unknown printer took a galley of type and scrambled it to make a type specimen\n			book.',NULL,'2019-02-02 06:08:54','2019-02-02 06:08:54');
+INSERT INTO `project` (`ID`,`Reference`,`Name`,`Description`,`Symbol`,`CreatedOn`,`UpdatedOn`) VALUES (5,'3b6c165e-26b1-11e9-bbf8-1215b6b719c2','Project Bajaaj','Lorem Ipsum is simply dummy text of the printing and typesetting\n			industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500 s,\n			when an unknown printer took a galley of type and scrambled it to make a type specimen\n			book.',NULL,'2019-02-02 06:09:14','2019-02-02 06:09:14');
+INSERT INTO `project` (`ID`,`Reference`,`Name`,`Description`,`Symbol`,`CreatedOn`,`UpdatedOn`) VALUES (7,'640967f7-26b1-11e9-bbf8-1215b6b719c2','Project School','Lorem Ipsum is simply dummy text of the printing and typesetting\n			industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500 s,\n			when an unknown printer took a galley of type and scrambled it to make a type specimen\n			book.',NULL,'2019-02-02 06:10:21','2019-02-02 06:10:21');
+INSERT INTO `project` (`ID`,`Reference`,`Name`,`Description`,`Symbol`,`CreatedOn`,`UpdatedOn`) VALUES (8,'0861b903-2713-11e9-bbf8-1215b6b719c2','Test','dffd',NULL,'2019-02-02 00:00:00','2019-02-02 00:00:00');
+INSERT INTO `project` (`ID`,`Reference`,`Name`,`Description`,`Symbol`,`CreatedOn`,`UpdatedOn`) VALUES (9,'4fc66dc5-2713-11e9-bbf8-1215b6b719c2','Test1','dffd',NULL,'2019-02-02 00:00:00','2019-02-02 00:00:00');
+INSERT INTO `project` (`ID`,`Reference`,`Name`,`Description`,`Symbol`,`CreatedOn`,`UpdatedOn`) VALUES (10,'ca9f9a42-2713-11e9-bbf8-1215b6b719c2','Test10','dffd',NULL,'2019-02-02 00:00:00','2019-02-02 00:00:00');
+INSERT INTO `project` (`ID`,`Reference`,`Name`,`Description`,`Symbol`,`CreatedOn`,`UpdatedOn`) VALUES (11,'00d91e9d-2714-11e9-bbf8-1215b6b719c2','Test100','dffd',NULL,'2019-02-02 00:00:00','2019-02-02 00:00:00');
+INSERT INTO `project` (`ID`,`Reference`,`Name`,`Description`,`Symbol`,`CreatedOn`,`UpdatedOn`) VALUES (12,'2bf59e3e-2714-11e9-bbf8-1215b6b719c2','Test1000','dffd',NULL,'2019-02-02 00:00:00','2019-02-02 00:00:00');
+INSERT INTO `project` (`ID`,`Reference`,`Name`,`Description`,`Symbol`,`CreatedOn`,`UpdatedOn`) VALUES (13,'094524f8-2715-11e9-bbf8-1215b6b719c2','Test1020','dffd',NULL,'2019-02-02 18:03:32','2019-02-02 18:03:32');
+
+
+
+
+/*
+-- Query: SELECT * FROM test.forms
+-- Date: 2019-02-03 20:22
+*/
+INSERT INTO `forms` (`ID`,`Reference`,`Name`,`SymbolID`,`CreatedOn`,`UpdatedOn`) VALUES (7,'dcf9c009-26b3-11e9-bbf8-1215b6b719c2','Form XYZ',1,'2019-02-02 06:28:02','2019-02-02 06:28:02');
+INSERT INTO `forms` (`ID`,`Reference`,`Name`,`SymbolID`,`CreatedOn`,`UpdatedOn`) VALUES (8,'dcf9f837-26b3-11e9-bbf8-1215b6b719c2','Form MNO',1,'2019-02-02 06:28:02','2019-02-02 06:28:02');
+INSERT INTO `forms` (`ID`,`Reference`,`Name`,`SymbolID`,`CreatedOn`,`UpdatedOn`) VALUES (9,'dcf9f9a3-26b3-11e9-bbf8-1215b6b719c2','Form XYW',2,'2019-02-02 06:28:02','2019-02-02 06:28:02');
+INSERT INTO `forms` (`ID`,`Reference`,`Name`,`SymbolID`,`CreatedOn`,`UpdatedOn`) VALUES (10,'dcf9fa3f-26b3-11e9-bbf8-1215b6b719c2','Form PQR',2,'2019-02-02 06:28:02','2019-02-02 06:28:02');
+INSERT INTO `forms` (`ID`,`Reference`,`Name`,`SymbolID`,`CreatedOn`,`UpdatedOn`) VALUES (11,'dcf9fabf-26b3-11e9-bbf8-1215b6b719c2','Form ABC',2,'2019-02-02 06:28:02','2019-02-02 06:28:02');
+INSERT INTO `forms` (`ID`,`Reference`,`Name`,`SymbolID`,`CreatedOn`,`UpdatedOn`) VALUES (12,'dcf9fb40-26b3-11e9-bbf8-1215b6b719c2','Form QWERTY',2,'2019-02-02 06:28:02','2019-02-02 06:28:02');
+
+
+
+
+
+/*
+-- Query: SELECT * FROM test.projectforms
+-- Date: 2019-02-03 20:24
+*/
+INSERT INTO `projectforms` (`ID`,`Reference`,`ProjectID`,`FormID`,`CreatedOn`,`UpdatedOn`) VALUES (3,'2f7ec813-26b1-11e9-bbf8-1215b6b719c2',1,7,'2019-02-02 06:02:53','2019-02-02 06:02:53');
+INSERT INTO `projectforms` (`ID`,`Reference`,`ProjectID`,`FormID`,`CreatedOn`,`UpdatedOn`) VALUES (4,'caa0177e-2713-11e9-bbf8-1215b6b719c2',10,7,'2019-02-02 00:00:00','2019-02-02 00:00:00');
+INSERT INTO `projectforms` (`ID`,`Reference`,`ProjectID`,`FormID`,`CreatedOn`,`UpdatedOn`) VALUES (5,'00d9c8c1-2714-11e9-bbf8-1215b6b719c2',11,7,'2019-02-02 00:00:00','2019-02-02 00:00:00');
+INSERT INTO `projectforms` (`ID`,`Reference`,`ProjectID`,`FormID`,`CreatedOn`,`UpdatedOn`) VALUES (6,'2bf68e31-2714-11e9-bbf8-1215b6b719c2',12,7,'2019-02-02 00:00:00','2019-02-02 00:00:00');
+INSERT INTO `projectforms` (`ID`,`Reference`,`ProjectID`,`FormID`,`CreatedOn`,`UpdatedOn`) VALUES (7,'0945760f-2715-11e9-bbf8-1215b6b719c2',13,7,'2019-02-02 18:03:32','2019-02-02 18:03:32');
+
+
+
+/*
+-- Query: SELECT * FROM test.projectusers
+-- Date: 2019-02-03 20:24
+*/
+INSERT INTO `projectusers` (`ID`,`Reference`,`ProjectID`,`UserProfileID`,`CreatedOn`,`UpdatedOn`) VALUES (1,'2bfa5327-2714-11e9-bbf8-1215b6b719ce',1,1,'2019-02-02 06:28:02','2019-02-02 06:28:02');
+INSERT INTO `projectusers` (`ID`,`Reference`,`ProjectID`,`UserProfileID`,`CreatedOn`,`UpdatedOn`) VALUES (2,'2bfa5327-2714-11e9-bbf8-1215b6b719cp',1,2,'2019-02-02 06:28:02','2019-02-02 06:28:02');
+INSERT INTO `projectusers` (`ID`,`Reference`,`ProjectID`,`UserProfileID`,`CreatedOn`,`UpdatedOn`) VALUES (3,'2bfa5327-2714-11e9-bbf8-1215b6b719c2',12,2,'2019-02-02 00:00:00','2019-02-02 00:00:00');
+INSERT INTO `projectusers` (`ID`,`Reference`,`ProjectID`,`UserProfileID`,`CreatedOn`,`UpdatedOn`) VALUES (4,'09458398-2715-11e9-bbf8-1215b6b719c2',13,2,'2019-02-02 18:03:32','2019-02-02 18:03:32');
+
+
